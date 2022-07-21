3a022099f2990f7dd820bba50ccef491136fdd0e
m <m@mm1.local>
2022-07-21 17:05:42 +0800

    新增物流更新的函数

--- lib/PayPal/Api/Trackers.php
+++ lib/PayPal/Api/Trackers.php
@@ -29,6 +29,7 @@ class Trackers extends PayPalResourceModel
     {
         return $this->trackers;
     }
+
     /**
      * An array of tracking information for shipments..
      *
@@ -41,6 +42,7 @@ class Trackers extends PayPalResourceModel
         $this->trackers = $trackers;
         return $this;
     }
+
     /**
      * Append tracking information to this list.
      *
@@ -57,6 +59,7 @@ class Trackers extends PayPalResourceModel
             );
         }
     }
+
     /**
      * errors for the batch_add state.
      *
@@ -66,6 +69,7 @@ class Trackers extends PayPalResourceModel
     {
         return $this->errors;
     }
+
     /**
      * errors for the batch_add state.
      *
@@ -75,12 +79,13 @@ class Trackers extends PayPalResourceModel
     {
         return $this->errors;
     }
+
     /**
      * Creates and processes a payment. In the JSON request body, include a `payment` object with the intent, payer, and transactions. For PayPal payments, include redirect URLs in the `payment` object.
      *
      * @param ApiContext $apiContext is the APIContext for this call. It can be used to pass dynamic configuration and credentials.
      * @param PayPalRestCall $restCall is the Rest Call Service that is used to make rest calls
-     * @return Payment
+     * @return Trackers
      */
     public function batch($apiContext = null, $restCall = null)
     {
@@ -95,6 +100,28 @@ class Trackers extends PayPalResourceModel
         );
         $this->fromJson($json);
         return $this;
+    }
+
+    /**
+     * Updates or cancels the tracking information for a PayPal transaction, by ID. To cancel tracking information, call this method and set the status to CANCELLED. For more information, see Update or cancel tracking information.  *
+     * @param ApiContext $apiContext is the APIContext for this call. It can be used to pass dynamic configuration and credentials.
+     * @param PayPalRestCall $restCall is the Rest Call Service that is used to make rest calls
+     * @return Trackers
+     */
+    public function update($track_number_new, $apiContext = null, $restCall = null)
+    {
+        $obj_arr = $this->getTrackers();
+        $obj = array_pop($obj_arr);
+        $id = $obj->getTransactionId() . '-' . $obj->getTrackingNumber();
+        $obj->setTrackingNumber($track_number_new);
+        return self::executeCall(
+            "/v1/shipping/trackers/$id",
+            "PUT",
+            $obj->toJSON(),
+            null,
+            $apiContext,
+            $restCall
+        );
     }
 
 }
